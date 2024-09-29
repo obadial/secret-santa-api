57355bbed03e6f58ba271e60cb6d49c7527a90f8
lucas obadia <52819806+obadial@users.noreply.github.com>
2024-09-29 23:18:58 +0200

    Clean code (#8)

--- app/utils/list_utils.py
+++ app/utils/list_utils.py
@@ -18,12 +18,10 @@ def get_default_list(session: Session) -> SecretSantaList:
     """
     This function retrieves or creates the default Secret Santa list if it doesnt exist.
     """
-    # Try to find the default list
     default_list = session.exec(
         select(SecretSantaList).where(SecretSantaList.name == DEFAULT_LIST_NAME)
     ).first()
 
-    # If not found, create the default list
     if not default_list:
         default_list = SecretSantaList(name=DEFAULT_LIST_NAME)
         session.add(default_list)

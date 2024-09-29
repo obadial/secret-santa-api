ac817e82692374d07b6f11672c396c46fce9ad74
Lucas Obadia <lucas.obadia@gmail.com>
2024-09-29 16:58:40 +0200

    test

--- app/models/secretsantalist.py
+++ app/models/secretsantalist.py
@@ -1,12 +1,10 @@
 from sqlmodel import SQLModel, Field
 from sqlalchemy.orm import relationship
 from typing import List, Optional
-from app.models.participant import Participant
-from app.models.blacklist import Blacklist
 
 
 class SecretSantaList(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    participants: List[Participant] = relationship("Participant", back_populates="list")
-    blacklists: List[Blacklist] = relationship("Blacklist", back_populates="list")
+    participants: List["Participant"] = relationship("Participant", back_populates="list")
+    blacklists: List["Blacklist"] = relationship("Blacklist", back_populates="list")
